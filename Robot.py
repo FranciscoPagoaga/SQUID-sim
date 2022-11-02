bd1a68bcd195735c82cffa15ccf21c1fd10cfed6
Francisco Pagoaga <francisco.pagaoga14@gmail.com>
2022-11-02 15:52:51 -0600

    carga el mapa y lo mete dentro de clase robot

---
+++ Robot.py
@@ -0,0 +1,39 @@
+import sys
+
+class Robot:
+    def __init__(self, fileName ):
+        self.pos_x = -1
+        self.pos_y = -1
+        self.pos_z = 0
+        #File opening to assign map coordinate map
+        self.coordinateMap = self.loadMap(fileName)
+
+    def loadMap(self, fileName):
+        rowCounter= 0
+        columnCounter=0
+        coordinateMap = []
+        with open(fileName, "r") as file:
+            info = file.readlines()
+            firstRun =  True
+            for line in info:
+                line = line.strip()
+                #Loading dimensions of the map
+                if firstRun:
+                   dimensions = line.split(",") 
+                #    coordinateMap[int(dimensions[0])][int(dimensions[1])]
+                   print(dimensions[0], dimensions[1])
+                   firstRun=False
+                else:
+                #Fill map with its matrices
+                    row = []
+                    for char in list(line):
+                        row.append(char)
+                        #Searching for the 'R' to know the starting coordinate
+                        if char == "R":
+                            self.pos_x = rowCounter
+                            self.pos_y = columnCounter
+                        columnCounter+=1
+                    columnCounter=0
+                    rowCounter+=1
+                    coordinateMap
+        return coordinateMap
