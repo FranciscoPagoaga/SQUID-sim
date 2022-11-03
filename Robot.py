39e61c0612ee329a5e947c7f68d4e0a30539d48a
Francisco Pagoaga <francisco.pagaoga14@gmail.com>
2022-11-03 12:31:23 -0600

    Movement func advance and coords x,y switch

--- Robot.py
+++ Robot.py
@@ -2,52 +2,62 @@ import sys
 from StateNode import StateNode
 from DeliveryState import DeliveryState
 
+
 class Robot:
-    def __init__(self, fileNameMap, fileNameDeliveries ):
-        self.posX = -1
+    def __init__(self, fileNameMap, fileNameDeliveries):
         self.posY = -1
+        self.posX = -1
         self.posZ = 0
         self.maximumCost = 0
-        #File opening to assign map coordinate map
+
+        # Border for future validations
+        self.borderX = 0
+        self.borderY = 0
+        # File opening to assign map coordinate map
         lines = self.readMap(fileNameMap)
         self.coordinateMap = self.loadMap(lines)
 
-        #File opening to create delivery list
+        print(self.borderX, self.borderY)
+
+        # File opening to create delivery list
         lines = self.readMap(fileNameDeliveries)
         tmpDeliveries = self.loadDeliveries(lines)
 
-        #Root state node creation
-        self.rootNode = StateNode(0,None,tmpDeliveries)
+        # Root state node creation
+        self.rootNode = StateNode(
+            0, None, tmpDeliveries, None, self.posY, self.posX, self.posZ)
 
     def readMap(self, fileName):
         with open(fileName, "r") as file:
             info = file.readlines()
         return info
-    
+
     def loadMap(self, info):
-        rowCounter= 0
-        columnCounter=0
+        rowCounter = 0
+        columnCounter = 0
         coordinateMap = []
-        firstRun =  True
+        firstRun = True
         for line in info:
             line = line.strip()
-            #Loading dimensions of the map
+            # Loading dimensions of the map
             if firstRun:
-                dimensions = line.split(",") 
-                firstRun=False
+                dimensions = line.split(",")
+                self.borderY = dimensions[0]
+                self.borderX = dimensions[1]
+                firstRun = False
             else:
-            #Fill map with its matrices
+                # Fill map with its matrices
                 row = []
                 for char in list(line):
                     row.append(char)
-                    #Searching for the 'R' to know the starting coordinate
+                    # Searching for the 'R' to know the starting coordinate
                     if char == "R":
-                        self.posX = rowCounter
-                        self.posY = columnCounter
-                    columnCounter+=1
-                columnCounter=0
-                rowCounter+=1
-                #Appends the whole row
+                        self.posY = rowCounter
+                        self.posX = columnCounter
+                    columnCounter += 1
+                columnCounter = 0
+                rowCounter += 1
+                # Appends the whole row
                 coordinateMap.append(row)
         return coordinateMap
 
@@ -57,31 +67,72 @@ class Robot:
         for line in info:
             line = line.strip()
             if isFirst:
-                #Sets a maximum cost for later on searches 
+                # Sets a maximum cost for later on searches
                 self.maximumCost = int(line)
                 isFirst = False
             else:
-                #Calls functino to return created object
+                # Calls functino to return created object
                 deliveryList.append(self.createDeliveryState(line))
-        return deliveryList 
-    
+        return deliveryList
+
     def createDeliveryState(self, line):
-        #String management to extract the coordinates and types
-        line = line.split("-") 
-        #Extracts the load and drop type
-        loadDestination= line[0]
+        # String management to extract the coordinates and types
+        line = line.split("-")
+        # Extracts the load and drop type
+        loadDestination = line[0]
         dropDestination = line[2]
-        #split the coordinates
+        # split the coordinates
         PosList = line[1].split(",")
-        #Skips the first parenthesis inside the splitted result
-        loadPosX = int((PosList[0])[1:])
-        loadPosY = int(PosList[1]) 
-        #Skips the last parenthesis inside the splitted result                
-        loadPosZ = int((PosList[2])[:-1])             
+        # Skips the first parenthesis inside the splitted result
+        loadposY = int((PosList[0])[1:])
+        loadposX = int(PosList[1])
+        # Skips the last parenthesis inside the splitted result
+        loadPosZ = int((PosList[2])[:-1])
         PosList = line[3].split(",")
-        #Skips the first parenthesis inside the splitted result                
-        dropPosX = int((PosList[0])[1:])
-        dropPosY = int(PosList[1]) 
-        #Skips the last parenthesis inside the splitted result                
+        # Skips the first parenthesis inside the splitted result
+        dropposY = int((PosList[0])[1:])
+        dropposX = int(PosList[1])
+        # Skips the last parenthesis inside the splitted result
         dropPosZ = int((PosList[2])[:-1])
-        return DeliveryState(loadDestination, loadPosX, loadPosY, loadPosZ, dropDestination, dropPosX, dropPosY, dropPosZ)
+        return DeliveryState(loadDestination, loadposY, loadposX, loadPosZ, dropDestination, dropposY, dropposX, dropPosZ)
+
+    # def BreadthFirstSearch(self):
+
+    # This validates if the move could be valid
+
+    def isMoveValid(self, axis, value):
+        # If the axis is X it checks if the move is valid by checking the border
+        if (axis == "y"):
+            if ((self.posY + value) < self.borderX and (self.posY + value) >= 0):
+                #Condition to validate if the move has a Border, and obstacle, or is up on the shelf
+                if  (self.coordinateMap[self.posY+value][self.posX] == "B" or
+                    self.coordinateMap[self.posY+value][self.posX] == "X" or 
+                    self.coordinateMap[self.posY+value][self.posX].isNumeric() or
+                    self.posZ > 0):
+                    return False
+                else:
+                    return True
+            else:
+                return False
+        elif (axis == "x"):
+            if ((self.posX + value) < self.borderY and (self.posX + value) >= 0):
+
+                return True
+            else:
+                return False
+        elif (axis == "z"):
+            if  (self.coordinateMap[self.posY+1][self.posX] == "B" and  
+                (self.posZ + value < self.getShelfLevels() or 
+                self.posZ + value >0)):
+                return True
+            else:
+                return False
+                
+    def getShelfLevels(self):
+        #Gets how many levels the shelf has by cheking either y+1 and x+1 or x-1
+        if(self.coordinateMap[self.PosY+1][self.posX+1].isNumeric()):
+            return int(self.coordinateMap[self.PosY+1][self.posX+1])
+        elif(self.coordinateMap[self.PosY+1][self.posX-1].isNumeric()):
+            return int(self.coordinateMap[self.PosY+1][self.posX-1])
+        else:
+            return -1
